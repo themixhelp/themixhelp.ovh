3b046214e7beb7f9abb15f85a524763a9bb03322
themixhelp <themixhelp@outlook.com>
2023-01-20 00:42:51 +0100

    add scss

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <script setup></script>
 
-<template></template>
+<template>
+	<section class='app-wrapper'>
+		<h1>themixhelp!</h1>
+	</section>
+</template>
 
-<style scoped></style>
+<style lang='scss' scoped>
+	.app-wrapper {}
+</style>
