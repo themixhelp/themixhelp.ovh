87f7a6a8b210c34421467145138a7953140ab77d
themixhelp <themixhelp@outlook.com>
2023-01-20 01:37:25 +0100

    add basic landing view

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <script setup></script>
 
 <template>
-	<section class='app-wrapper'>
-		<h1>themixhelp!</h1>
+	<section class="app-wrapper">
+		<h1>
+            themixhelp<span class="highlight">.</span>
+        </h1>
 	</section>
 </template>
 
-<style lang='scss' scoped>
-	.app-wrapper {}
+<style lang="scss" scoped>
+    @use '@/styles/variables.scss' as *;
+
+	.app-wrapper {
+		width: 100vw;
+		height: 100vh;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+        h1 {
+            font-size: clamp(2rem, 10vw, 5rem);
+            font-weight: 700;
+
+            text-align: center;
+        }
+	}
 </style>
