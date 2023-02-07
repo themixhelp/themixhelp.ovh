96cd4dec61c27f6a8fd2a128c0b8454d41d9a386
themixhelp <themixhelp@outlook.com>
2023-02-07 14:05:57 +0100

    create content in AboutMe.vue

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -3,9 +3,23 @@
 <template>
 	<section class="about-me-wrapper">
 		<h1>about-me</h1>
+
+		<article>
+			<p>hey! i'm Mikołaj, but you probably know me as <strong>themixhelp</strong>.</p>
+			<p>i'm generally interested in computer science, but i mainly work in <strong>webdev</strong>! i'm also supporting <strong>open-source</strong> projects so most of my codes are available on github.</p>
+		</article>
+
+		<article>
+			<p>do you want to contact me? write to <strong>themixhelp@outlook.com</strong>.</p>
+			<p>prefer another way? you can do it via discord: <strong>themixhelp#3576</strong>!</p>
+		</article>
 	</section>
 </template>
 
 <style lang="scss" scoped>
 	@use '@/styles/sections.scss';
+
+	article {
+		margin-bottom: 0.75em;
+	}
 </style>
